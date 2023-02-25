6d7aac8fb335257c9aebc52a18f547a8da148449
tushar.paliwal <tushar.paliwal@deliveryhero.in>
2023-02-25 13:43:24 +0530

    Creates core structure for application.

---
+++ src/main/java/com/mywhoosh/studentresultcompilation/StudentResultCompilationApplication.java
@@ -0,0 +1,13 @@
+package com.mywhoosh.studentresultcompilation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultCompilationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentResultCompilationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mywhoosh/studentresultcompilation/StudentResultCompilationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywhoosh.studentresultcompilation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentResultCompilationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
