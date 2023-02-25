3d0feac2aa4a67a46b7ad3ef67b4d54f0841e320
tushar.paliwal <tushar.paliwal@deliveryhero.in>
2023-02-25 16:04:23 +0530

    Adds repository for student document.

---
+++ src/main/java/com/mywhoosh/studentresultcompilation/repository/StudentRepository.java
@@ -0,0 +1,20 @@
+package com.mywhoosh.studentresultcompilation.repository;
+
+import com.mywhoosh.studentresultcompilation.enums.StudentStatusEnum;
+import com.mywhoosh.studentresultcompilation.model.Student;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+/**
+ * Repository for managing student document.
+ */
+@Repository
+public interface StudentRepository extends ReactiveMongoRepository<Student, String> {
+
+    Flux<Student> findAllByStudentStatus(StudentStatusEnum status);
+    Mono<Student> findByIdAndStudentStatus(String id, StudentStatusEnum status);
+
+}
