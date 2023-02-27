de2e171bcd96abc91192f96fbecaf838d9bf483b
tushar.paliwal <tushar.paliwal@deliveryhero.in>
2023-02-27 14:14:48 +0530

    Adds security properties for testcases.

--- src/test/java/com/mywhoosh/studentresultcompilation/service/ResultServiceTests.java
+++ src/test/java/com/mywhoosh/studentresultcompilation/service/ResultServiceTests.java
@@ -23,6 +23,7 @@ import reactor.test.StepVerifier;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.when;
